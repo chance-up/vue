a425b4957a35fa354b33cce500074831cd2ff86e
chance-up <ccs6473@naver.com>
2022-05-23 17:31:26 +0900

    feat:main component

---
+++ vue2-typescript/src/components/MainComponent.vue
@@ -0,0 +1,17 @@
+<template lang="">
+  <div>
+    <h1>this is main component</h1>
+  </div>
+</template>
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class MainComponent extends Vue {
+  @Prop({ default: "Hello World!" }) message!: string;
+  created() {
+    console.log("MainComponent created");
+  }
+}
+</script>
+<style lang=""></style>
