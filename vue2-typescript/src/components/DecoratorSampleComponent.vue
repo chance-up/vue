dd9ccb5e0321513fb4d749717cdef76210c396a6
chance-up <ccs6473@naver.com>
2022-05-31 17:33:03 +0900

    feat: decorator 예제에서 사용할 컴포넌트 추가

---
+++ vue2-typescript/src/components/DecoratorSampleComponent.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>This is ClassComponentSamplePage !</h1>
+    <h2>{{ message }}</h2>
+    <input type="text" v-model="message" />
+    <button @click="changeMessage">Change Message</button>
+  </div>
+</template>
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class DecoratorSampleComponent extends Vue {}
+</script>
+<style lang=""></style>
