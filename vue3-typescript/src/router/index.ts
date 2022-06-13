4ea3668d3c73d03826075c04644a3dc88d0f92d8
chance-up <ccs6473@naver.com>
2022-06-13 10:24:02 +0900

    Feat:vue router @문법 잘 안되서 설정 추가

--- vue3-typescript/src/router/index.ts
+++ vue3-typescript/src/router/index.ts
@@ -1,12 +1,12 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHistory } from 'vue-router';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: "/",
-      name: "main",
-      component: () => import("@/views/MainView.vue"),
+      path: '/',
+      name: 'main',
+      component: () => import('@/views/MainView.vue'),
     },
   ],
 });
