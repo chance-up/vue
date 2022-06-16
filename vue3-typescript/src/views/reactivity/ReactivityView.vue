89b14e5b2d24ef52a8f8876fbc163267732ad213
chance-up <ccs6473@naver.com>
2022-06-16 17:53:42 +0900

    feat:reactive and ref test

---
+++ vue3-typescript/src/views/reactivity/ReactivityView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div></div>
+</template>
+
+<script setup lang="ts">
+import { ref, reactive } from 'vue';
+
+const refText = ref('this is exText');
+const reacriveText = reactive({
+  exText: refText,
+});
+</script>
+
+<style scoped></style>
